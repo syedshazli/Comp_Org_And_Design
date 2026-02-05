4effb252b4585d0209e0d10f619c5ce9f778acb0
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-02-05 14:38:03 -0500

    done

--- HW2/HW2.asm
+++ HW2/HW2.asm
@@ -1,1 +1,5 @@
-
+sum:
+  # TODO: Add a bne instruction for the while loop, comparing i to input n
+  addi $sp, $sp, -8 # make space for i and sum
+# TODO: save on the stock
+  jr $ra # jump to return address
