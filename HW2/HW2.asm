df80d9e7a52cfeaa8910608b6669c0fdc4e52d95
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-02-09 16:09:58 -0500

    loaded i and sum from stack into regs

--- HW2/HW2.asm
+++ HW2/HW2.asm
@@ -1,5 +1,11 @@
 sum:
   # TODO: Add a bne instruction for the while loop, comparing i to input n
   addi $sp, $sp, -8 # make space for i and sum
+  sw $t0, 4($sp) # save register $t0 for use afterwards (For sum)
+  sw $s0, 0($sp) # save register $s0 for use afterwards (for i)
+
+  addi $t0, $t0, $zero
+  addi $s0, $s0, $zero
+
 # TODO: save on the stock
   jr $ra # jump to return address
