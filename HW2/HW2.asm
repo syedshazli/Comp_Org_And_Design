8c556da981ed74c727b59cc115f9764e0c8900ff
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-02-10 14:05:49 -0500

    TODO: finish loop

--- HW2/HW2.asm
+++ HW2/HW2.asm
@@ -4,10 +4,16 @@ sum:
   sw $t0, 4($sp) # save register $t0 for use afterwards (For sum)
   sw $s0, 0($sp) # save register $s0 for use afterwards (for i)
 
-  addi $t0, $t0, $zero
-  addi $s0, $s0, $zero
+  addi $t0, $t0, $zero # initialize i to 0
+  addi $s0, $s0, $zero # initialize sum to 0
 
+  j loop
 
+loop:
+  be $t0, a1,exit # exit if i == n
+  j loop # i != n, jump back to the loop
+
+exit:
   add $v0, $t0, $zero # store return type sum in return value register
   lw $t0, 4($sp) # restore registers to previous values
   lw $s0, 0($sp) # restore registers to previous values
