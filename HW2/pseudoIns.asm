1117b46c33363f07d4317d3d37f71b718a00ec3d
Syed Shazli <146783525+syedshazli@users.noreply.github.com>
2026-02-14 10:28:47 -0500

    done

--- HW2/pseudoIns.asm
+++ HW2/pseudoIns.asm
@@ -9,8 +9,8 @@ ble:
   beq $at, $zero, Label # if t5 was not less than t3, then t5 >= t3, branch
 
 bgt:
-  slt $at, $t3, $t5 # if t3 is greater than t5, then $t1 is set to 0.
-  bne $at, 1, Label # 0 != 1, branch!
+  slt $at, $t5, $t4 # if t5 is greater than or equal to t4, then $at is set to 0, otherwise $at is set to 1.
+  bne $at, $zero, Label # 0 != 1, branch!
 
 bge: # want to check if t5 >= t3
   slt $at, $t5, $t3 # is t5 less than t3? if so, set $t1 to 1
@@ -24,7 +24,7 @@ beq:
 # BEQ FOR LARGE CONSTANT
 beq:
   lui $at, BIG_UPPER_16 # UPPER 16 BITS OF BIG
-  ori $at, BIG_LOWER_16 # LOWER 16 BITS OF BIG
+  ori $at, $at, BIG_LOWER_16 # LOWER 16 BITS OF BIG
   beq $t1, $at, Label
 
 # LI SMALL
@@ -33,18 +33,18 @@ li:
 
 # LI BIG
 li:
-  lui $t2, BIG # UPPER 16 BITS OF BIG
-  ori $t2, BIG # LOWER 16 BITS OF BIG
+  lui $t2, BIG_UPPER_16 # UPPER 16 BITS OF BIG
+  ori $t2, $t2, BIG_LOWER_16 # LOWER 16 BITS OF BIG
   
 # ADDI BIG
 addi:
-  lui $t0, BIG # UPPER 16 BITS OF BIG
-  ori $t0, BIG # LOWER 16 BITS OF BIG
+  lui $t0, BIG_UPPER_16 # UPPER 16 BITS OF BIG
+  ori $t0, $t0, BIG_LOWER_16 # LOWER 16 BITS OF BIG
   add $t0, $t0, $t2
 
 # LW BIG
 lw:
   lui $at, BIG_UPPER_16 # load the upper part of constant into $t4
-  ori $at, BIG_LOWER_16 $ load the lower part of $t4 into 
+  ori $at, $at, BIG_LOWER_16 $ load the lower part of $t4 into 
   add $at, $t2, $at # add upper part of $t3
   lw $t5, 0($at)
